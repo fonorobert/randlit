11e44a43fd1a48aa093569b3023d2f6fa5e61fe9
Robert Fono <fonorobert@gmail.com>
2018-08-16 20:51:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# randlit
+
+Pick a random piece of literature from a database of texts and print it to an ESC/POS thermal printer
+
+## v0.1 [WIP]
+
+- ingest text
+- pick random piece of text
+- print it to `stdout`
+
+
+## Notes
+
+Needs to pick from database for easier handling of text records
+Needs a util to parse a txt file and create database records from it for each piece of text
---
+++ randlit.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+// Poem struct
+
+type LitStruct struct {
+	author string
+	title string
+	body string
+}
+
+// func ReadFile(path string) content string {
+
+// 	// Open file from `path`, read text from it
+// }
+
+// func ParseText(input string, separators []string) texts[]string {
+
+// 	// Split text from single string into slice by separators
+// }
+
+// func ParsePoems(rawPoems []string) (poems []LitStruct) {
+
+// 	// Split poems into map of author, title and poem body
+
+// 	// for _,poem := range rawPoems {
+// 	// 	poems = append(poems, LitStruct{author: "John Boyega", title: "Untitled poem", body: poem})
+// 	// }
+
+// 	// return poems
+// }
+
+
+func main() {
+
+	fmt.Println("hello world")
+
+}
